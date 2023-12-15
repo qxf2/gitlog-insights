6c79360a72f19b96941d5656c0131e81521980b2
Mohanqxf2 <mohan@qxf2.com>
2023-12-15 15:10:05 +0530

    updated the script to print the Author bias

--- insights/author_bias_insights.py
+++ insights/author_bias_insights.py
@@ -27,6 +27,81 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from modules import fetch_author_count
 
 
+# def get_inputs():
+#     """
+#     Prompts the user to enter a start date, end date, repository path.
+
+#     Returns: A tuple containing the start date, end date, repository path.
+#     """
+
+#     while True:
+#         try:
+#             start_date_input = input("Enter the start date (YYYY-MM-DD): ")
+#             break
+#         except ValueError:
+#             print(
+#                 "Invalid start date format. Please enter a valid date in YYYY-MM-DD format."
+#             )
+
+#     while True:
+#         try:
+#             end_date_input = input("Enter the end date (YYYY-MM-DD): ")
+#             if end_date_input > start_date_input:
+#                 break
+#             print("End date must be greater than start date. Please try again.")
+#         except ValueError:
+#             print("Invalid date format. Please try again.")
+
+#     repo_path_input = input("Enter the repository path (For example:https://github.com/qxf2/qxf2-page-object-model.git): ")
+
+#     return start_date_input, end_date_input, repo_path_input
+
+
+# def write_html_report(file_info_df, file_name):
+#     """
+#     Writes a DataFrame to an HTML report file.
+
+#     Args:
+#         df (DataFrame): The DataFrame containing the data to be written to the HTML report.
+#         file_name (str): The name of the file to which the HTML report will be written.
+
+#     Returns:
+#         None
+#     """
+#     try:
+#         with open(file_name, "w", encoding='utf-8') as file:
+#             if file_info_df.empty:
+#                 message = "No data available between the specified dates."
+#                 file.write(message)
+#             else:
+#                 html = file_info_df.to_html(index=False)
+#                 file.write(html)
+#     except (FileNotFoundError, PermissionError) as error:
+#         print(f"An error occurred while writing the HTML report: {str(error)}")
+
+
+# if __name__ == "__main__":
+#     start_date, end_date, repo_path = get_inputs()
+#     contributors_data = fetch_author_count.get_contributors_info(
+#         repo_path, start_date, end_date
+#     )
+
+#     # Find the maximum major value
+#     max_major = contributors_data['Major'].max()
+
+#     # Find all rows with the maximum major value
+#     highest_major_rows = contributors_data[contributors_data['Major'] == max_major]
+
+
+#     # Print information for all rows with the highest major value
+#     print("Files with the Highest Major:", len(highest_major_rows))
+#     for index, row in highest_major_rows.iterrows():
+        
+#         print(f"File {row['File Name']} biased by {row['Major']} author")
+
+#     write_html_report(contributors_data, "report.html")
+
+
 def get_inputs():
     """
     Prompts the user to enter a start date, end date, repository path.
@@ -57,6 +132,24 @@ def get_inputs():
     return start_date_input, end_date_input, repo_path_input
 
 
+def calculate_author_bias(major):
+    """
+    Calculates the author bias level based on the "Major" value.
+
+    Args:
+        major (int): The value of the "Major" column.
+
+    Returns:
+        str: Author bias level (Low, Moderate, High).
+    """
+    if major <= 2:
+        return "High"
+    elif major <= 3:
+        return "Moderate"
+    else:
+        return "Low"
+
+
 def write_html_report(file_info_df, file_name):
     """
     Writes a DataFrame to an HTML report file.
@@ -74,6 +167,8 @@ def write_html_report(file_info_df, file_name):
                 message = "No data available between the specified dates."
                 file.write(message)
             else:
+                # Add a new column for Author Bias
+                file_info_df['Author Bias'] = file_info_df['Major'].apply(calculate_author_bias)
                 html = file_info_df.to_html(index=False)
                 file.write(html)
     except (FileNotFoundError, PermissionError) as error:
@@ -86,17 +181,12 @@ if __name__ == "__main__":
         repo_path, start_date, end_date
     )
 
-    # Find the maximum major value
-    max_major = contributors_data['Major'].max()
+    # Find rows with High major value
+    high_author_bias_rows = contributors_data[contributors_data['Major'] <= 2]
 
-    # Find all rows with the maximum major value
-    highest_major_rows = contributors_data[contributors_data['Major'] == max_major]
-
-
-    # Print information for all rows with the highest major value
-    print("Files with the Highest Major:", len(highest_major_rows))
-    for index, row in highest_major_rows.iterrows():
-        
+    # Print information for all rows with High major value
+    print("Files with High Author Bias:")
+    for index, row in high_author_bias_rows.iterrows():
         print(f"File {row['File Name']} biased by {row['Major']} author")
 
     write_html_report(contributors_data, "report.html")
