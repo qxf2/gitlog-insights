0f33199d1c90376ac208592878d596fc0ce6ddcf
Sravanti <sravanti.tatiraju@qxf2.com>
2024-01-10 13:54:51 +0530

    Corrected PR number displayed in insights, fixed few pylint warnings

--- insights/size_of_prs.py
+++ insights/size_of_prs.py
@@ -2,7 +2,6 @@
 This script is used to produce the following insight:
 Find the size of the PR and number of files changed in a GitHub repository 
 within the specified time period.
-This info can help testing teams align their testing efforts accordingly.
 
 Usage:
 python size_of_PRs.py
@@ -11,11 +10,11 @@ Provide the following inputs:
 Enter the start date in YYYY-MM-DD format (eg: 2023-02-01)
 Enter the end date in YYYY-MM-DD format (must be greater than start date) (eg: 2023-02-08)
 Enter the repository path: local or remote GitHub repositories
-(eg: https://github.com/qxf2/newsletter_automation.git)
+(eg: qxf2/newsletter_automation.git)
 
 - The script prompts for necessary inputs and
 then fetches the max size of the PR and max files changed  within the specified date range.
-It displays the results in the form of a simple html page.
+It displays the inference and detailed results in form of a simple html page.
 
 Outputs:
 The max PR size and Max files modified in the repository based on the number of modifications.
@@ -24,18 +23,17 @@ The max PR size and Max files modified in the repository based on the number of 
 import os
 import sys
 from datetime import datetime
-
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from modules import fetch_size_of_pr
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from helpers import github_pr_data_extractor
-
 from utils import logger_util
+
 logger_util.setup_logging()
 logger = logger_util.get_logger("userLogger")
 
 gitlog_insights_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+reports_dir = os.path.join(gitlog_insights_dir, 'reports')
+html_report_path = os.path.join(reports_dir, 'size_of_prs_report.html')
 
 def get_inputs():
     """
@@ -88,22 +86,23 @@ def write_html_report(file_info_df,file_name):
             else:
                 html = file_info_df.to_html(index=False)
                 file.write(html)
-                text = "The best practice is to have 200-400 LOC per PR."
-                text += " This extensive change might pose challenges in review and testing"
+                text = "\nThe best practice is to have 200-400 LOC per PR."
                 file.write(text)
-    except (FileNotFoundError, PermissionError) as err:
-        print(f"An error occurred while writing the HTML report: {str(err)}")
+    except (FileNotFoundError, PermissionError) as report_error:
+        logger.error("An error occurred while writing the HTML report: %s", report_error)
+        sys.exit(1)
 
 if __name__ == "__main__":
     start_date, end_date, repo_path = get_inputs()
     try:
         pr_details = fetch_size_of_pr.get_pr_details(repo_path,start_date,end_date)
+
     except github_pr_data_extractor.PRDataExtractionError as error:
         error_message = f"Error extracting PR details for repository \
             '{repo_path}' between {start_date} and {end_date}: {error}"
         logger.error(error_message)
         sys.exit(1)
     if not pr_details.empty:
-        combined_df = fetch_size_of_pr.get_pr_insights(pr_details)
-        write_html_report(combined_df, "insights_for_size_of_pr.html")
-        print('\nDetailed log analysis can be found in insights_for_size_of_pr.html\n')
+        fetch_size_of_pr.get_pr_insights(pr_details)
+        write_html_report(pr_details, html_report_path)
+        print('\nDetailed report can be found in size_of_prs_report.html\n')
--- modules/fetch_size_of_pr.py
+++ modules/fetch_size_of_pr.py
@@ -4,9 +4,12 @@ This script fetches the PR details for a given time period
 import sys
 import os
 import pandas as pd
-
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from helpers.github_pr_data_extractor import PRDataExtractor
+from helpers.github_pr_data_extractor import PRDataExtractor, PRDataExtractionError
+from utils import logger_util
+
+logger = logger_util.get_logger("root")
+
 
 def get_pr_details(repo_name, start_date, end_date):
     """
@@ -17,49 +20,54 @@ def get_pr_details(repo_name, start_date, end_date):
         github_api = PRDataExtractor(repo_name)
         pr_details = github_api.get_pr_files_details(start_date, end_date)
         if pr_details.empty:
+            print(
+                f"No data found betweent the specified dates : {start_date} and {end_date}"
+            )
             return pd.DataFrame()
-        grouped_pr_details = pr_details.groupby('pr_number').agg({
-            'filename': 'nunique',  # Number of unique files changed
-            'changes': 'sum'        # Total number of lines changed
-        }).reset_index()
-        grouped_pr_details.columns = ['pr_number', 'num_files_changed', 'total_lines_changed']
-        print("Size of each pull request:")
+        grouped_pr_details = (
+            pr_details.groupby("pr_number")
+            .agg(
+                {
+                    "filename": "nunique",  # Number of unique files changed
+                    "changes": "sum",  # Total number of lines changed
+                }
+            )
+            .reset_index()
+        )
+        grouped_pr_details.columns = [
+            "pr_number",
+            "num_files_changed",
+            "total_lines_changed",
+        ]
         return grouped_pr_details
-    except Exception as error:
-        print(f"An error occurred: {error}")
-        return pd.DataFrame()
+
+    except ValueError as value_error:
+        logger.exception("An error occured while extracting PR data %s", value_error)
+        raise PRDataExtractionError("Error while extracting PR data") from value_error
+    except KeyError as key_error:
+        logger.exception("An error occured while extracting PR data %s", key_error)
+        raise PRDataExtractionError("Error while extracting PR data") from key_error
 
 def get_pr_insights(pr_details):
     "PR insights"
-    number_of_lines_changed = pr_details.loc[pr_details['total_lines_changed'].idxmax()]
-    pr_number = number_of_lines_changed['pr_number']
-    total_lines_changed = number_of_lines_changed['total_lines_changed']
-      # Creating DataFrames
-    lines_changed_df = pd.DataFrame({
-        "PR_Number": [number_of_lines_changed['pr_number']],
-        "Total_Lines_Changed": [number_of_lines_changed['total_lines_changed']]
-    })
+    number_of_lines_changed = pr_details.loc[pr_details["total_lines_changed"].idxmax()]
+    pr_number = number_of_lines_changed["pr_number"]
+    total_lines_changed = number_of_lines_changed["total_lines_changed"]
+
     # Framing inferences
     lines_message = (
-    f"\n -> The PR#{pr_number} has maximum number of code lines changed: "
-    f"{total_lines_changed} lines. \nThis extensive change might pose challenges "
-    "in review and testing."
+        f"\n -> The PR#{pr_number} has maximum number of code lines changed: "
+        f"{total_lines_changed} lines."
     )
-    print(f"\nInsights for PR :")
+    print("\nInsights for the time period:")
     print(lines_message)
-    number_of_files_changed = pr_details.loc[pr_details['num_files_changed'].idxmax()]
-    num_files_changed = number_of_files_changed['num_files_changed']
-    files_changed_df = pd.DataFrame({
-        "PR_Number": [number_of_files_changed['pr_number']],
-        "Num_Files_Changed": [number_of_files_changed['num_files_changed']]
-    })
-    message = (
-    f"\n -> The PR#{pr_number} has maximum number of files changed: "
-    f"{num_files_changed} changes.\n This needs more attention towards "
-    "review and testing"
-)
-    print (message)
-    # Merging DataFrames
-    combined_df = pd.merge(lines_changed_df, files_changed_df, on='PR_Number', how='outer')
 
-    return combined_df
+    number_of_files_changed = pr_details.loc[pr_details["num_files_changed"].idxmax()]
+    pr_number_files = number_of_files_changed["pr_number"]
+    num_files_changed = number_of_files_changed["num_files_changed"]
+
+    files_message = (
+        f"\n -> The PR#{pr_number_files} has maximum number of files changed: "
+        f"{num_files_changed} changes."
+    )
+    print(files_message)
